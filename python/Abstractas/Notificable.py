0c84f7a6a6e2af045062ef2a4d8dbcfc3dd2f946
H MarBel <husaimb22@gmail.com>
2025-11-30 10:42:23 -0600

    python service docs

--- python/Abstractas/Notificable.py
+++ python/Abstractas/Notificable.py
@@ -2,10 +2,23 @@
 
 from abc import ABC, abstractmethod
 
+
 class Notificable(ABC):
     """
-    Clase abstracta para objetos que pueden enviar notificaciones.
+    Define la interfaz para cualquier objeto del sistema que deba tener la
+    capacidad de enviar notificaciones al usuario, a otros servicios o a consolas.
+    Esta es una clase base abstracta.
     """
     @abstractmethod
     def enviar_notificacion(self):
+        """
+        Método abstracto que permite a las subclases a implementar la lógica
+        específica de envío de una notificación.
+        
+        Parámetros:
+            Ninguno
+        
+        Returns:
+            None
+        """
         pass
