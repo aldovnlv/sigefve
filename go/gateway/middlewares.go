ad4125b59df22f340abac786561af79bcf2cf686
aldovnlv <aldovnlv@gmail.com>
2025-12-01 02:08:34 -0600

    persistencia

--- go/gateway/middlewares.go
+++ go/gateway/middlewares.go
@@ -17,6 +17,24 @@ var claveJWT = []byte("mi_clave_super_secreta")
 // Rate limitador: 3 requests por segundo
 var limitador = rate.NewLimiter(3, 3)
 
+
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
+}
+
+
 // JWT Middleware
 func ConectorAutenticacion() gin.HandlerFunc {
 	return func(c *gin.Context) {
