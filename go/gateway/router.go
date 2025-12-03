e02090e9074724ca1e60e195939f42cedbcf49c4
Aldo Ivan <aldovnlv@gmail.com>
2025-12-03 15:02:00 -0600

    orginal

--- go/gateway/router.go
+++ go/gateway/router.go
@@ -35,7 +35,7 @@ func (router Router) PrepararRutas() *gin.Engine {
 		AllowMethods:     []string{"POST", "GET", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
+		AllowCredentials: false,
 		MaxAge:           12 * time.Hour,
 	}))
 
